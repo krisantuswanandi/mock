25ced901b909fbbe9a28bd8769c090c92470182e
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-02-09 11:00:53 +0700

    update vercel config

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT
 const app = express()
 app.use(cors())
 
-app.use('/disbursement', auth, disbursement)
+app.use('/api/disbursement', auth, disbursement)
 
 app.listen(PORT, function () {
   console.log(`Listening on port ${PORT}`)
